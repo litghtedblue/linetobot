7fcceaf75e9948fe38355173bf138fcf6e5ec230
tuyo <lightedblue@gmail.com>
2016-11-05 20:16:59 +0900

    post

--- index.js
+++ index.js
@@ -15,19 +15,39 @@ app.post('/', function(req, res) {
     var d=dump(v);
     console.log(d);
 
-    var text=v.events[0].message.text;
-    console.log(text);
-    var token=v.events[0].replyToken;
-    console.log(token);
-
     v.events.forEach(function(num){
       var postpara={
         text:num.message.text,
         replyToken:num.replyToken
       }
-      postLine(postpara);
+      postReplyLine(postpara);
     });
     
+    res.send(d);
+});
+
+app.post('/sender', function(req, res) {
+    var v=req.body;
+    
+    // リクエストボディを出力
+    var d=dump(v);
+    console.log("request ip "+req.ip);
+    console.log(d);
+    
+    var reg = new RegExp(process.env.IP_REG);
+    if (!req.ip.match(reg)) {
+      console.log("ip not allow");
+      res.send(d);
+      return;
+    }
+
+    v.forEach(function(item){
+      var postpara={
+        text:item.text,
+      }
+      postPushLine(postpara); 
+    });
+        
     res.send(d);
 })
 
@@ -35,7 +55,46 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-function postLine(postpara){
+function postPushLine(postpara){
+  var request = require('request');
+  var post={
+      "to":process.env.USER_ID,
+      "messages":[
+          {
+              "type":"text",
+              "text":postpara.text
+          },
+      ]
+  };
+
+  var options = {
+    uri: 'https://api.line.me/v2/bot/message/push',
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json',
+      'Authorization':"Bearer "+process.env.LINE_CHANNEL_ACCESS_TOKEN
+    },
+    json: post,
+  };
+
+  console.log(dump(options));
+
+  if(process.env.NODE_ENV=="development"){
+    console.log("skip");
+    return;
+  }
+
+  request(options, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+      console.log("line reply ok");
+      console.log('line reply body: '+ dump(response.body));
+    } else {
+      console.log('line reply error: '+ response.statusCode);
+      console.log('line reply error: '+ dump(response.body));
+    }
+  });
+}
+function postReplyLine(postpara){
   var request = require('request');
   var post={
       "replyToken":postpara.replyToken,
@@ -59,6 +118,10 @@ function postLine(postpara){
 
   console.log(dump(options));
 
+  if(process.env.NODE_ENV=="development"){
+    console.log("skip");
+    return;
+  }
   request(options, function(error, response, body){
     if (!error && response.statusCode == 200) {
       console.log("line reply ok");
@@ -68,7 +131,6 @@ function postLine(postpara){
       console.log('line reply error: '+ dump(response.body));
     }
   });
-
 }
 function dump(v){
   return util.inspect(v,false,null);
