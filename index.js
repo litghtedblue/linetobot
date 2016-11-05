0aab2b66a1d2c468a86d1fd4c33ec1f6d5b40bcf
tuyo <lightedblue@gmail.com>
2016-11-05 13:03:33 +0900

    change

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 var express = require('express');
-var app = express();
+const bodyParser = require('body-parser');
 
+const app = express();
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// urlencodedとjsonは別々に初期化する
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.post('/', function(req, res) {
+    // リクエストボディを出力
+    console.log(req.body);
+    res.send('ok');
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
